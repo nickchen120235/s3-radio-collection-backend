8391c1d9c109d5b5b9d072874492486579e09513
Nickchen Nick <nickchen120235@gmail.com>
2024-02-18 10:43:53 +0800

    add token login

--- login.ts
+++ login.ts
@@ -1,18 +1,28 @@
-import { Router, createToken } from '/core.ts'
+import { Router, createToken, verifyToken } from '/core.ts'
 import { loadSync } from 'dotenv'
 
-const login = Router()
+type LoginData = { password: string } | { token: string }
+function isLoginData(data: any): data is LoginData {
+  return data.password !== undefined || data.token !== undefined
+}
 
+const login = Router()
 login.post('', async (req, res, next) => {
   try {
     const masterKey = Deno.env.get('PASSWORD') ?? loadSync().PASSWORD
-    const password = req.body.password
-    if (password === undefined)
+    if (!isLoginData(req.body))
       return res.status(400).json({ error: 'No password provided' })
-    if (password !== masterKey)
-      return res.status(401).json({ error: 'Invalid password' })
-    const token = await createToken()
-    return res.status(200).json({ token })
+    if ('password' in req.body) {
+      if (req.body.password !== masterKey)
+        return res.status(401).json({ error: 'Invalid password' })
+      const token = await createToken()
+      return res.status(200).json({ token })
+    }
+    else {
+      if (!await verifyToken(req.body.token))
+        return res.status(401).json({ error: 'Invalid authorization token' })
+      return res.status(200).json({ token: req.body.token })
+    }
   }
   catch (e) {
     next(e)
